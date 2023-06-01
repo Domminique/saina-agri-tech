46fbf6dec8c85697b7c0116907d3c18b61be2c0f
domminique <imbugad@gmail.com>
2023-06-01 23:49:14 +0300

    pricing model

--- src/shared/data.js
+++ src/shared/data.js
@@ -3,7 +3,7 @@ const reviews = [
     id: 1,
     name: 'Commission-based',
     job: '10% Commission ',
-    image: '/assets/images/oro5.png',
+    image: '/assets/images/chapchap12.png',
     text: 'saina takes a 10% of each sale made through the platform',
   },
 
@@ -11,14 +11,14 @@ const reviews = [
     id: 2,
     name: 'Subscription-based',
     job: 'From Ksh 500/Month or Ksh. 5500/Year',
-    image: '/assets/images/oro6.png',
+    image: '/assets/images/chapchap11.png',
     text: 'Agribusinesses and Smallholder farmers  pay a monthly or yearly subscription fee to access the marketplace to sell their products and services, manage IoT devices, access targeted and customized real-time information & insights',
   },
   {
     id: 2,
     name: 'Freemium',
     job: 'NGOs and Government ONLY',
-    image: '/assets/images/oro.png',
+    image: '/assets/images/chapchap15.png',
     text: ' Basic level of real-time information & insights for free, while charging for premium features or upgrades',
   },
 ]
